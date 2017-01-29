f70b6085347775b6aa4cae762938ca8d29cb4719
Roman Subik <roman.subik@ocado.com>
2017-01-29 00:17:34 +0000

    #15 Restructure project to simplify cron setup.

--- spiders/main.py
+++ spiders/main.py
@@ -3,15 +3,16 @@
 import sys
 
 from scrapy.crawler import CrawlerProcess
-# from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
-from spiders.spiders.otodom_spider import OtoDomSpider
+
 from elasticsearch.elasticsearch_helper import ElasticSearchHelper, MAPPING
 from notifier.sender import EmailSender
+from spiders.spiders.otodom_spider import OtoDomSpider
 
 mode = sys.argv[1]
 
 if __name__ == '__main__':
+
     es_helper = ElasticSearchHelper()
 
     if mode == 'init':
