a620c2e37ae80b565379560d7127e4413c63e292
RomekRJM <romek.rjm@gmail.com>
2017-02-12 23:20:01 +0000

    Start/Stop ElasticSearch server from cron.

--- run-me.sh
+++ run-me.sh
@@ -2,12 +2,25 @@
 
 HOUSE_FINDER_PATH=/home/odroid/house-finder/house-finder
 HOUSE_FINDER_VENV=/home/odroid/house-finder/venv
+ES_RUNABLE=/home/odroid/house-finder/elasticsearch-5.1.2/bin/elasticsearch
 
 export EMAIL_USER="your-user@test.com"
 export EMAIL_PASS="emailpass"
 export SMTP_SERVER="smtpserver"
 
-source "$HOUSE_FINDER_VENV/bin/activate"
-cd "$HOUSE_FINDER_PATH"
-python spiders/main.py $1
-deactivate
+if [ "$1" == 'start-es' ]
+then
+  $ES_RUNABLE &
+elif [ "$1" == 'stop-es' ]
+then
+  ES_PROCESS="$(ps -ef | grep elasticsearch | grep java | cut -d' ' -f 5)"
+  if [ ! -z "$ES_PROCESS" ]
+  then
+    kill -9 $ES_PROCESS
+  fi
+else
+  source "$HOUSE_FINDER_VENV/bin/activate"
+  cd "$HOUSE_FINDER_PATH"
+  python spiders/main.py $1
+  deactivate
+fi
